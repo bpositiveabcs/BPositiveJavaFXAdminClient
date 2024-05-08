ce79f8e955dee8b4b5047b615312b967761ff640
Deea <116511056+deeaion@users.noreply.github.com>
2024-05-08 14:02:18 +0300

    first commit

---
+++ src/main/java/bpos/adminclient/BPositiveAdminClientApplication.java
@@ -0,0 +1,13 @@
+package bpos.adminclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BPositiveAdminClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BPositiveAdminClientApplication.class, args);
+    }
+
+}
