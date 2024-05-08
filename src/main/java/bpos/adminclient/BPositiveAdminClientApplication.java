6a0a387e1716988d58db8a673bf0063a8dfdf824
Deea <116511056+deeaion@users.noreply.github.com>
2024-05-08 14:57:50 +0300

    added model dependency

--- src/main/java/bpos/adminclient/BPositiveAdminClientApplication.java
+++ src/main/java/bpos/adminclient/BPositiveAdminClientApplication.java
@@ -1,13 +1,20 @@
 package bpos.adminclient;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import bpos.common.model.Address;
 @SpringBootApplication
 public class BPositiveAdminClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BPositiveAdminClientApplication.class, args);
+    }
+    @PostConstruct
+    public void init()
+    {
+        Address address = new Address("aa","a","a","a","a","a",2,"a");
+        System.out.println(address.toString());
     }
 
 }
